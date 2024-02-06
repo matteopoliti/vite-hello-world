0345c857c0c300b796bf23a69bd2b180965a69de
matteopoliti <matteo.politi17@gmail.com>
2024-02-06 16:07:55 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "AppTitle"
+}
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: rgb(187, 0, 0);
+    text-align: center;
+    font-size: 50px;
+}
+</style>
